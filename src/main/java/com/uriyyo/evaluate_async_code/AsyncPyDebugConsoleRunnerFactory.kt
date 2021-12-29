9ed7da645dea5ddd0191aeb7135c7def157713c7
Yurii Karabas <1998uriyyo@gmail.com>
2021-12-29 16:40:54 +0200

    🔧 Disable createConsoleRunner

--- src/main/java/com/uriyyo/evaluate_async_code/AsyncPyDebugConsoleRunnerFactory.kt
+++ src/main/java/com/uriyyo/evaluate_async_code/AsyncPyDebugConsoleRunnerFactory.kt
@@ -7,42 +7,43 @@ import com.intellij.util.Consumer
 import com.jetbrains.python.console.*
 
 class AsyncPyDebugConsoleRunnerFactory : PydevConsoleRunnerFactory() {
-
-    override fun createConsoleRunner(
-            project: Project,
-            sdk: Sdk?,
-            workingDir: String?,
-            envs: MutableMap<String, String>,
-            consoleType: PyConsoleType, settingsProvider: PyConsoleOptions.PyConsoleSettings,
-            rerunAction: Consumer<in String>,
-            vararg setupFragment: String
-    ): PydevConsoleRunner {
-        return object : PydevConsoleRunnerImpl(
-                project,
-                sdk,
-                consoleType,
-                workingDir,
-                envs,
-                settingsProvider,
-                rerunAction,
-                *setupFragment
-        ) {
-            override fun createCommandLine(
-                    sdk: Sdk,
-                    environmentVariables: MutableMap<String, String>,
-                    workingDir: String?,
-                    port: Int
-            ): GeneralCommandLine =
-                    super.createCommandLine(sdk, environmentVariables, workingDir, port)
-                            .apply {
-                                sdk.whenSupport {
-                                    parametersList
-                                            .paramsGroups
-                                            .firstOrNull { it.id == "Script" }
-                                            ?.addPyDevAsyncWork()
-                                }
-                            }
-        }
-    }
+// FIXME: Resolve compatibility issue
+//
+//    override fun createConsoleRunner(
+//            project: Project,
+//            sdk: Sdk?,
+//            workingDir: String?,
+//            envs: MutableMap<String, String>,
+//            consoleType: PyConsoleType, settingsProvider: PyConsoleOptions.PyConsoleSettings,
+//            rerunAction: Consumer<in String>,
+//            vararg setupFragment: String
+//    ): PydevConsoleRunner {
+//        return object : PydevConsoleRunnerImpl(
+//                project,
+//                sdk,
+//                consoleType,
+//                workingDir,
+//                envs,
+//                settingsProvider,
+//                rerunAction,
+//                *setupFragment
+//        ) {
+//            override fun createCommandLine(
+//                    sdk: Sdk,
+//                    environmentVariables: MutableMap<String, String>,
+//                    workingDir: String?,
+//                    port: Int
+//            ): GeneralCommandLine =
+//                    super.createCommandLine(sdk, environmentVariables, workingDir, port)
+//                            .apply {
+//                                sdk.whenSupport {
+//                                    parametersList
+//                                            .paramsGroups
+//                                            .firstOrNull { it.id == "Script" }
+//                                            ?.addPyDevAsyncWork()
+//                                }
+//                            }
+//        }
+//    }
 
 }
